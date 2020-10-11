05b70241ccba13c85c9fb8f0d3c39cbfcc776b93
Akira Kobori <private.beats@gmail.com>
2020-10-11 20:24:22 +0900

    ニュースがない場合はTodoist連携しない

--- packers_news/controller.py
+++ packers_news/controller.py
@@ -20,7 +20,8 @@ def add_todoist_reading_list(task_title: str, news_list: NewsList):
     """
     Todoistの「リーディングリスト」に登録する
     """
-    todoist.add_reading_list(
-        parent_title=task_title,
-        sub_titles=news_list.to_todoist_titles(),
-        due="today")
+    if news_list.is_not_empty():
+        todoist.add_reading_list(
+            parent_title=task_title,
+            sub_titles=news_list.to_todoist_titles(),
+            due="today")
